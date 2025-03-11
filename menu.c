9c5984a113330fe2a07bb15d48e2d71a725b49b7
eunicean <eunice.mata2992@gmail.com>
2025-03-11 16:39:32 -0600

    base ready to use of menu

--- menu.c
+++ menu.c
@@ -5,10 +5,12 @@
 int main()
 {
     bool stay = true;
-    printf("WELCOME TO WTP CHAT \n");
+    printf("+-----------------------------------------------------------+\n");
+    printf("|                  WELCOME TO CHAT SERVER                   |\n");
+    printf("+-----------------------------------------------------------+\n");
     while(stay){
         int chatSelection;
-        printf("Select an action \n");
+        printf("\nSelect an action \n");
         printf(" 1. Chat globally\n");
         printf(" 2. Chat privately\n");
         printf(" 3. Change status\n");
@@ -22,24 +24,36 @@ int main()
         
         switch (chatSelection) {
             case 1:
-                printf("GLOBAL CHAT\n");
+                printf("+-----------------------------------------------------------+\n");
+                printf("|                        GLOBAL CHAT                        |\n");
+                printf("+-----------------------------------------------------------+\n");
+                // show all messages, every second refresh
+                // send messages
                 break;
             case 2:
-                printf("PRIVATE CHAT\n");
+                printf("+-----------------------------------------------------------+\n");
+                printf("|                       PRIVATE CHAT                        |\n");
+                printf("+-----------------------------------------------------------+\n");
                 // list all users available to chat
                 // select one user somehow
                 // send and receive chats
                 break;
             case 3:
-                printf("CHANGE STATUS\n");
+                printf("+-----------------------------------------------------------+\n");
+                printf("|                       CHANGE STATUS                       |\n");
+                printf("+-----------------------------------------------------------+\n");
                 // can change from active, ocuppied or inactive
                 break;
             case 4:
-                printf("LIST ALL USERS CONNECTED\n");
+                printf("+-----------------------------------------------------------+\n");
+                printf("|                 LIST ALL USERS CONNECTED                  |\n");
+                printf("+-----------------------------------------------------------+\n");
                 // self explanatory
                 break;
             case 5:
-                printf("SEE INFO ABOUT A USER\n");
+                printf("+-----------------------------------------------------------+\n");
+                printf("|                   SEE INFO ABOUT A USER                   |\n");
+                printf("+-----------------------------------------------------------+\n");
                 // list all users
                 // select one
                 // show username and ip address
@@ -48,7 +62,9 @@ int main()
                 int stillNeedsHelp = true;
                 while(stillNeedsHelp){
                     int helpSelection;
-                    printf("HELP SECTION\n");
+                    printf("\n+-----------------------------------------------------------+\n");
+                    printf("|                       HELP SECTION                        |\n");
+                    printf("+-----------------------------------------------------------+\n");
                     printf("Select the option where you want to get info about\n");
                     printf(" 1. Chat globally\n");
                     printf(" 2. Chat privately\n");
@@ -62,31 +78,53 @@ int main()
                     
                     switch(helpSelection){
                         case 1:
-                            printf("GLOBAL CHAT -----------------------------------------------\n");
+                            printf("\nGLOBAL CHAT -----------------------------------------------\n");
+                            printf("  Here you will be able to send an receive messages form\n");
+                            printf("  and to all the users connected in this global chat.\n");
+                            printf("  You can exit writing EXIT in the send message space.\n");
                             printf("-----------------------------------------------------------\n");
                             break;
                         case 2:
-                            printf("PRIVATE CHAT ----------------------------------------------\n");
+                            printf("\nPRIVATE CHAT ----------------------------------------------\n");
+                            printf("  Here you will be able to chat with privately with a user\n");
+                            printf("  that you can select from an available list.\n");
+                            printf("  You can exit writing EXIT in the send message space.\n");
                             printf("-----------------------------------------------------------\n");
                             break;
                         case 3:
-                            printf("CHANGE STATUS ---------------------------------------------\n");
+                            printf("\nCHANGE STATUS ---------------------------------------------\n");
+                            printf("  Here you will be able to change your status between the\n");
+                            printf("  options of ACTIVE, OCUPPIED or INACTIVE.\n");
+                            printf("  NOTE: Your default status is ACTIVE\n");
+                            printf("  NOTE: After some time innactive your status will change\n");
+                            printf("        to INACTIVE\n");
                             printf("-----------------------------------------------------------\n");
                             break;
                         case 4:
-                            printf("LIST ALL USERS CONNECTED ----------------------------------\n");
+                            printf("\nLIST ALL USERS CONNECTED ----------------------------------\n");
+                            printf("  Here you will be able to see all users connected to the\n");
+                            printf("  server\n");
                             printf("-----------------------------------------------------------\n");
                             break;
                         case 5:
-                            printf("SEE INFO ABOUT A USER -------------------------------------\n");
+                            printf("\nSEE INFO ABOUT A USER -------------------------------------\n");
+                            printf("  Here you will be able to select an user, and we will\n");
+                            printf("  show you the USERNAME and IP ADDRES of that user.\n");
+                            printf("  Maybe in the future we won't dox our users :)\n");
                             printf("-----------------------------------------------------------\n");
                             break;
                         case 6:
-                            printf("HELP SECTION ----------------------------------------------\n");
+                            printf("\nHELP SECTION ----------------------------------------------\n");
+                            printf("  Here you get information about the various things you\n");
+                            printf("  cand do on our server.\n");
                             printf("-----------------------------------------------------------\n");
                             break;
                         case 7:
-                            printf("EXIT ------------------------------------------------------\n");
+                            printf("\nEXIT ------------------------------------------------------\n");
+                            printf("  Here is where you LOG OUT, once you have logged out  all\n");
+                            printf("  your chats will dissapear. You can log in again, with\n");
+                            printf("  the same username or a different one to start chats\n");
+                            printf("  from zero\n");
                             printf("-----------------------------------------------------------\n");
                             break;
                         default:
@@ -103,6 +141,9 @@ int main()
                 
                 break;
             case 7:
+                printf("+-----------------------------------------------------------+\n");
+                printf("|                          LOG OUT                          |\n");
+                printf("+-----------------------------------------------------------+\n");
                 printf("Bye\n");
                 stay = false;
                 break;
